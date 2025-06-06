325e9c510bfd0af8c5a25bd7ad282ddb7875e79a
zslaposa <zsolt.laposa@yahoo.com>
2025-06-06 17:04:27 +0100

    parses arguments

--- headr/src/main.rs
+++ headr/src/main.rs
@@ -1,3 +1,52 @@
+use clap::{Arg, Command };
+
+#[derive(Debug)]
+struct Args {
+    files: Vec<String>,
+    lines: u64,
+    bytes: Option<u64>,
+}
+
+fn get_args() -> Args {
+    let arguments = Command::new("headr")
+        .version("0.1.0")
+        .about("Rust version of head")
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .num_args(0..)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::new("lines")
+                .value_name("LINES")
+                .short('n')
+                .long("lines")
+                .help("Number of lines")
+                .num_args(1)
+                .value_parser(clap::value_parser!(u64).range(1..))
+                .default_value("10"),
+        )
+        .arg(
+            Arg::new("bytes")
+                .short('c')
+                .long("bytes")
+                .value_name("BYTES")
+                .conflicts_with("lines")
+                .value_parser(clap::value_parser!(u64).range(1..))
+                .help("Number of bytes"),
+        )
+        .get_matches();
+
+    Args {
+        files: arguments.get_many("files").unwrap().cloned().collect(),
+        lines: arguments.get_one("lines").cloned().unwrap(),
+        bytes: arguments.get_one("bytes").cloned(),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = get_args();
+    println!("{:#?}", args);
 }
